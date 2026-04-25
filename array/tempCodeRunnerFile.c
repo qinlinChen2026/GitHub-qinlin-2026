61bd9fa6c71d6955a4414c20caf29b387eb84588
qinglin <19953517446@163.com>
2026-04-25 18:14:44 +0800

    完成sort和count

--- array/tempCodeRunnerFile.c
+++ array/tempCodeRunnerFile.c
@@ -1,57 +1,29 @@
-// 题目1
-// 从键盘输入 10 个整数 存入一维数组，要求：
-// 把数组中的元素按从小到大排序（用选择排序）
-// 输出排序后的数组
-// 再输入一个整数 key，在数组中查找 key 是否存在
-// 如果存在，输出它第一次出现的下标；不存在则输出 -1
+// 题目2：统计数组中每个数字出现的次数
+// 输入 10 个整数 到一维数组中，每个数的范围在 0~9 之间。编程统计 0~9 找到出现次数最多的数  并输出 出现几次 比如 打印  数字 5 出现了 3次
 #include <stdio.h>
 int main()
 {
     int a[10];
-    int i, j, temp, key, MinIndex;
+    int i;
+    int count[10] = {0};
+    int MaxNum = 0;
+    int MaxCnt = 0;
     printf("输入10个整数\n");
-    // 取10个整数数组
     for (i = 0; i < 10; i++)
     {
         scanf("%d", &a[i]);
     }
-    // 冒泡排序
-    for (i = 0; i < 9; i++)
+    for (i = 0; i < 10; i++)
     {
-        MinIndex = i;
-        for (j = i + 1; j < 10; j++)
-        {
-            if (a[j] < MinIndex)
-            {
-                MinIndex = j;
+        count[a[i]]++;
 
-                temp = a[i];
-                a[i] = a[MinIndex];
-                a[MinIndex] = temp;
-            }
-        }
-    }
-    printf("打印排序后的数组：\n");
-    for (i = 0; i < 10; i++)
-    {
-        printf("%d\n", a[i]);
-    }
-    printf("输入查找的再输入一个整数key\n");
-    scanf("%d", &key);
-    int isFound = 0;
-    for (i = 0; i < 10; i++)
-    {
-        if (a[i] == key)
+        if (count[i] > MaxCnt)
         {
-            printf("%d\n", i);
-            isFound = 1;
-            break;
+            MaxCnt = count[i];
+            MaxNum = i;
         }
     }
-    if (isFound == 0)
-    {
-        printf("不存在则输出:%d\n", -1);
-    }
+    printf("出现次数最多的数 %d 出现%d次 ", MaxNum, MaxCnt);
 
     return 0;
 }
