6b35e34bcdcd5882579ece418b41e0f6f9bbcd98
qinglin <19953517446@163.com>
2026-04-27 16:41:57 +0800

    完成递归

--- recursion/reversePrint.c
+++ recursion/reversePrint.c
@@ -4,4 +4,29 @@
 // 递归思路
 // 终止条件：数字 n < 10，直接打印自己
 // 递归逻辑：
-// 先递归打印 n / 10 剩下的高位，再打印当前最后一位 n % 10
+// 先递归打印 n / 10 剩下的高位，再打印当前最后一位 n % 10、
+#include <stdio.h>
+
+// 递归函数：逆序打印数字
+void reversePrint(int n)
+{
+    // 终止条件：n < 10，直接打印
+    if (n < 10)
+    {
+        printf("%d", n);
+        return;
+    }
+    reversePrint(n / 10);
+    printf("%d", n % 10);
+}
+
+int main()
+{
+    int n;
+    printf("请输入一个正整数：");
+    scanf("%d", &n);
+
+    printf("逆序结果：");
+    reversePrint(n);
+    return 0;
+}
